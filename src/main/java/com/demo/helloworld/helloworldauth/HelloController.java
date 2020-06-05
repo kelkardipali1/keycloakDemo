b965b3b7229fb6fbe1eb0ec4ee62dc47450efb32
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2020-06-05 20:10:31 +0530

    [Dipali] Add. keycloak integreted in demo project

--- src/main/java/com/demo/helloworld/helloworldauth/HelloController.java
+++ src/main/java/com/demo/helloworld/helloworldauth/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/payments")
 public class HelloController {
 
     @PostMapping("/hello")
